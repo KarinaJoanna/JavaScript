16c5d1f63e33eb623f781913de588f6ef1758019
Karina Joanna <karina.joanna@hotmail.com>
2022-10-25 18:30:19 -0500

    v0

---
+++ ejercicios/ejercicio7/script.js
@@ -0,0 +1,15 @@
+function abrirRegalo (event){
+
+    const image = event.currentTarget;
+    image.removeEventListener('click', abrirRegalo);
+
+    const abierto = document.querySelector('#regalo-abierto');
+    const cerrado = document.querySelector('#regalo-cerrado');
+
+    abierto.classList.remove('oculto');
+    cerrado.classList.add('oculto');
+
+}
+
+const image = document.querySelector('#regalo-cerrado img');
+image.addEventListener('click', abrirRegalo);
